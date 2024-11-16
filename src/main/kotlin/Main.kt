8aa6472f5b06b81e1bfae8d53c0788115aa33ac9
JohnnyGomezzz <seldesign@mail.ru>
2024-11-16 12:15:33 +0300

    KTB-07 show learning menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,32 +12,23 @@ fun main() {
         println(
             """
                 |
-        |Меню:
-        |1 - Учить слова
-        |2 - Статистика
-        |0 - Выход
-    """.trimMargin()
+                |Меню:
+                |1 - Учить слова
+                |2 - Статистика
+                |0 - Выход
+            """.trimMargin()
         )
 
         val choice = readln()
         when (choice) {
-            "1" -> println("Вы выбрали \"Учить слова\"")
+            "1" -> {
+                println("Вы выбрали \"Учить слова\"")
+                getLearning(dictionary)
+            }
+
             "2" -> {
                 println("Вы выбрали \"Статистика\"")
-
-                val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }
-                val learnedCount = learnedWords.size
-                val totalCount = dictionary.size
-                val percent = learnedCount.toDouble() / totalCount.toDouble() * 100.0
-
-                println(
-                    String.format(
-                        "Выучено %d из %d слов | %d%%",
-                        learnedCount,
-                        totalCount,
-                        percent.roundToInt()
-                    )
-                )
+                getStatistics(dictionary)
             }
 
             "0" -> return
@@ -46,8 +37,51 @@ fun main() {
     }
 }
 
-fun loadDictionary(wordsFile: File): MutableList<Word> {
+fun getLearning(dictionary: MutableList<Word>) {
+    val notLearnedList = dictionary.filter { it.correctAnswersCount < 3 }
 
+    while (notLearnedList.isNotEmpty()) {
+        val questionWords = notLearnedList.shuffled().take(4)
+        val correctAnswer = questionWords.shuffled()[0]
+
+        println(
+            String.format(
+                """
+                                |
+                                |%s:
+                                |1 - %s
+                                |2 - %s
+                                |3 - %s
+                                |4 - %s
+                            """.trimMargin(),
+                correctAnswer.original,
+                questionWords[0].translate,
+                questionWords[1].translate,
+                questionWords[2].translate,
+                questionWords[3].translate,
+            )
+        )
+        val userAnswer = readln()
+    }
+}
+
+fun getStatistics(dictionary: MutableList<Word>) {
+    val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }
+    val learnedCount = learnedWords.size
+    val totalCount = dictionary.size
+    val percent = learnedCount.toDouble() / totalCount.toDouble() * 100.0
+
+    println(
+        String.format(
+            "Выучено %d из %d слов | %d%%",
+            learnedCount,
+            totalCount,
+            percent.roundToInt()
+        )
+    )
+}
+
+fun loadDictionary(wordsFile: File): MutableList<Word> {
     val dictionary: MutableList<Word> = mutableListOf()
 
     val lines: List<String> = wordsFile.readLines()
