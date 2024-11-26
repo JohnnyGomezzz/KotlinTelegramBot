77f1594f5e521870f0c6a22f5ce75867346796f0
JohnnyGomezzz <seldesign@mail.ru>
2024-11-26 18:32:30 +0300

    KTB-14 update id and text parsed

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -14,11 +14,18 @@ fun main(args: Array<String>) {
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        updateId = updateIdString.toInt() + 1
+        val updateIdRegex: Regex = "\"update_id\":(\\d+),".toRegex()
+        val idMatchResult: MatchResult = updateIdRegex.find(updates) ?: continue
+        val idGroups = idMatchResult.groups
+        updateId = idGroups[1]?.value?.toInt()!! + 1
+
+        val messageTextRegex: Regex = "\"text\":\"(.+)\"".toRegex()
+        val matchResult: MatchResult? = messageTextRegex.find(updates)
+        val groups = matchResult?.groups
+        val text = groups?.get(1)?.value
+
+        println(updateId)
+        println(text)
     }
 }
 
