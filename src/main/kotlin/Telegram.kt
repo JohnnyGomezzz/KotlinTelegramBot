cb916a5417e753d935f673b7933d48b9ed782376
JohnnyGomezzz <seldesign@mail.ru>
2024-12-09 18:35:38 +0300

    KTB-21 migration complete

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,27 +1,72 @@
 package org.example
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class Update(
+    @SerialName("update_id")
+    val updateId: Long,
+    @SerialName("message")
+    val message: Message? = null,
+    @SerialName("callback_query")
+    val callbackQuery: CallbackQuery? = null,
+)
+
+@Serializable
+data class Response(
+    @SerialName("result")
+    val result: List<Update>,
+)
+
+@Serializable
+data class Message(
+    @SerialName("text")
+    val text: String? = null,
+    @SerialName("chat")
+    val chat: Chat,
+)
+
+@Serializable
+data class CallbackQuery(
+    @SerialName("data")
+    val data: String,
+    @SerialName("message")
+    val message: Message? = null,
+)
+
+@Serializable
+data class Chat(
+    @SerialName("id")
+    val id: Long,
+)
+
 fun main(args: Array<String>) {
     val service = TelegramBotService(args[0])
     val trainer = LearnWordsTrainer()
-    var updateId = 0
+    var lastUpdateId = 0L
 
-    val updateIdRegex: Regex = "\"update_id\":(\\d+),".toRegex()
-    val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
-    val chatIdRegex: Regex = "\"chat\":\\{\"id\":(-*\\d+),".toRegex()
-    val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
+    val json = Json {
+        ignoreUnknownKeys = true
+    }
 
     while (true) {
         Thread.sleep(2000)
-        val updates: String = service.getUpdates(updateId)
-        println(updates)
+        val responseString: String = service.getUpdates(lastUpdateId)
+        println(responseString)
+        val response: Response = json.decodeFromString(responseString)
+        val updates = response.result
+        val firstUpdate = updates.firstOrNull() ?: continue
+        val updateId = firstUpdate.updateId
+        lastUpdateId = updateId + 1
 
-        updateId = getFromUpdates(updateIdRegex, updates)?.toIntOrNull()?.plus(1) ?: continue
-        val chatId = getFromUpdates(chatIdRegex, updates)?.toLongOrNull() ?: continue
-        val receivedText = getFromUpdates(messageTextRegex, updates)
-        val receivedData = getFromUpdates(dataRegex, updates)
+        val chatId = firstUpdate.message?.chat?.id ?: firstUpdate.callbackQuery?.message?.chat?.id
+        val receivedText = firstUpdate.message?.text
+        val receivedData = firstUpdate.callbackQuery?.data
 
-        if (receivedText == "/start".lowercase()) service.sendMenu(chatId)
-        if (receivedData == STATISTICS_CLICKED) {
+        if (receivedText == "/start".lowercase() && chatId != null) service.sendMenu(chatId)
+        if (receivedData == STATISTICS_CLICKED && chatId != null) {
             val statistics = trainer.getStatistics()
             service.sendMessage(
                 chatId,
@@ -33,9 +78,9 @@ fun main(args: Array<String>) {
                 )
             )
         }
-        if (receivedData == LEARN_WORDS_CLICKED) checkNextQuestionAndSend(trainer, service, chatId)
-        if (receivedData == BACK_TO_MENU) service.sendMenu(chatId)
-        if (receivedData?.startsWith(CALLBACK_DATA_ANSWER_PREFIX) == true) {
+        if (receivedData == LEARN_WORDS_CLICKED && chatId != null) checkNextQuestionAndSend(trainer, service, chatId)
+        if (receivedData == BACK_TO_MENU && chatId != null) service.sendMenu(chatId)
+        if (receivedData?.startsWith(CALLBACK_DATA_ANSWER_PREFIX) == true && chatId != null) {
             val dataIndex = receivedData.substringAfter("_").toInt()
             if (trainer.checkAnswer(dataIndex)) {
                 service.sendMessage(
@@ -61,10 +106,4 @@ fun checkNextQuestionAndSend(trainer: LearnWordsTrainer, service: TelegramBotSer
         )
         service.sendMenu(chatId)
     } else service.sendQuestion(chatId, question)
-}
-
-fun getFromUpdates(dataRegex: Regex, updates: String): String? {
-    val matchResult: MatchResult? = dataRegex.find(updates)
-    val groups = matchResult?.groups
-    return groups?.get(1)?.value
 }
