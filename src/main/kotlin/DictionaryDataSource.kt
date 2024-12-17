768fbd4bdd6637e3180a346650d86669827f4134
JohnnyGomezzz <seldesign@mail.ru>
2024-12-18 00:13:38 +0300

    dictionary added to db

--- src/main/kotlin/DictionaryDataSource.kt
+++ src/main/kotlin/DictionaryDataSource.kt
@@ -1,8 +1,14 @@
 package org.example
 
+import java.io.File
 import java.sql.DriverManager
 
 fun main() {
+    val wordsFile = File("words.txt")
+    updateDictionary(wordsFile)
+}
+
+fun updateDictionary(wordsFile: File) {
     DriverManager.getConnection("jdbc:sqlite:data.db")
         .use { connection ->
             val statement = connection.createStatement()
@@ -12,10 +18,21 @@ fun main() {
                     'id' integer PRIMARY KEY AUTOINCREMENT UNIQUE,
                     'text' varchar UNIQUE,
                     'translate' varchar,
-                    'correct_answers_count' varchar 
+                    'correct_answers_count' integer 
                 );
             """.trimIndent()
             )
-            statement.executeUpdate("insert into words values(null, \'cat\', \'кошка\', 0)")
+            wordsFile.readLines().forEach {
+                val line = it.split("|")
+                statement.executeUpdate(
+                    String.format(
+                        "insert into words values(null, \'%s\', \'%s\', %d)",
+                        line[0],
+                        line[1],
+                        line.getOrNull(2)?.toIntOrNull() ?: 0,
+                    )
+
+                )
+            }
         }
 }
